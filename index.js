70032b6153d9e92fd7ee80d1c83ad9588786bcc3
tyaaadewiii <komangsintyadewi04@gmail.com>
2025-03-20 16:56:59 +0800

    nginstall express dan membuat API dengan Express

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import http from 'http';
+import express from 'express';
 import mysql from 'mysql2';
 
 const db = mysql.createConnection({ host: "localhost", user: "root", database: "openapi", password: ""});
+const app = express();
 
-const server = http.createServer((req, res) => {
-    db.query('SELECT * FORM user', (err, results) => {
-        if(err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain'});
-            res.end('Internal Server Error');
+app.get('/users', (req,res) => {
+    db.query('SELECT * FROM user', (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
             return;
         }
 
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(results));
+        res.json(results);
     });
 });
-server.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
+app.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
