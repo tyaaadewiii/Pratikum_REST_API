6948bfc8e58f8ab9e976149867ecbc0383d7049f
tyaaadewiii <komangsintyadewi04@gmail.com>
2025-03-20 18:05:12 +0800

    menambahkan endpoint API menggunakan nodejs (get, delete, put, dan post)

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ const swaggerDocument = YAML.parse(fs.readFileSync('./user_api.yaml', 'utf8'));
 
 const db = mysql.createConnection({ host: "localhost", user: "root", database: "openapi", password: ""});
 const app = express();
+app.use(express.json());
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/users', (req,res) => {
-    db.query('SELECT * FROM user', (err, results) => {
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.query('SELECT * FROM users WHERE id = ?', [id], (err, results) => {
         if (err) {
             res.status(500).send('Internal Server Error');
             return;
@@ -23,5 +35,47 @@ app.get('/users', (req,res) => {
         res.json(results);
     });
 });
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.query('DELETE FROM users WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = req.query.name;
+    const email = req.query.email;
+    const age = req.query.age;
+    db.query('UPDATE users SET name = ?, email = ?, age = ? WHERE id = ?', [name, email, age, id], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.post('/user', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const age = req.body.age;
+    db.query('INSERT INTO users (name, email, age) VALUES (?, ?, ?)', [name, email, age], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
 app.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
 
