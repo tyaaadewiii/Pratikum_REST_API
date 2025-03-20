a8a1a4aa96cd0449fecdfc935a77f629594814c2
tyaaadewiii <komangsintyadewi04@gmail.com>
2025-03-20 17:37:20 +0800

    menampilkan swagger UI dari openapi spec

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import mysql from 'mysql2';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import YAML from 'yaml';
+
+const swaggerDocument = YAML.parse(fs.readFileSync('./user_api.yaml', 'utf8'));
+
+
 
 const db = mysql.createConnection({ host: "localhost", user: "root", database: "openapi", password: ""});
 const app = express();
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/users', (req,res) => {
     db.query('SELECT * FROM user', (err, results) => {
@@ -15,3 +24,4 @@ app.get('/users', (req,res) => {
     });
 });
 app.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
+
