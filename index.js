3eb7486a6db0081c74023a90fe42b4adfba47731
tyaaadewiii <komangsintyadewi04@gmail.com>
2025-03-20 16:15:43 +0800

    mengkoneksi database mysql pada node.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import http from 'http';
+import mysql from 'mysql2';
+
+const db = mysql.createConnection({ host: "localhost", user: "root", database: "openapi", password: ""});
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!');
+    db.query('SELECT * FORM user', (err, results) => {
+        if(err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain'});
+            res.end('Internal Server Error');
+            return;
+        }
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(results));
+    });
 });
 server.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
